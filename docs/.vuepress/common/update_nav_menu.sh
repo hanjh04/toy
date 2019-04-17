3b2115df6e71e18da14a28533a08b18ab9889a27
한장호 <jangho21@naver.com>
2019-04-17 23:44:02 +0900

    Update update_nav_menu.sh

--- docs/.vuepress/common/update_nav_menu.sh
+++ docs/.vuepress/common/update_nav_menu.sh
@@ -6,36 +6,34 @@ CONFIG_FILE=${CONFIG_FILE_DIR}"config.js"
 TMP_CONFIG_FILE=${BASE_DIR}".vuepress/common/config/tmp_config.js"
 IGNORE_DIR="public"
 
-# copy file
-# cp config/tmp_config.js config/config.js
+# copy backup file to actual file
 cp ${TMP_CONFIG_FILE} ${CONFIG_FILE}
 
 FOLDERS=`ls -l ${BASE_DIR} | grep ^d | awk '{print $9}'`
-for folder in ${FOLDERS}
-do
-    if [ "${folder}" != "public" ];
-    # if [ "${folder}" != "${IGNORE_DIR}" ];
-    then
+for folder in ${FOLDERS}; do
+    if [ "${folder}" != "${IGNORE_DIR}" ];then
         TARGET_DIR=${BASE_DIR}${folder}"/"
         FILE_NAMES=`ls -l ${TARGET_DIR} | grep ^- | awk '{print $9}'`
-        for FILE_NAME_WITH_EXT in ${FILE_NAMES}
-        do
+
+        for FILE_NAME_WITH_EXT in ${FILE_NAMES}; do
             # split FILE_NAME_WITH_EXT and get FILE_NAME and EXT
             OIFS=$IFS;IFS=.;FILE_NAME_WITH_EXT_SPLIT=($FILE_NAME_WITH_EXT);IFS=$OIFS
             FILE_NAME=${FILE_NAME_WITH_EXT_SPLIT[0]}
             EXT=${FILE_NAME_WITH_EXT_SPLIT[1]}
 
-            if [ "${FILE_NAME}" != "index" ];
-            then
+            if [ "${FILE_NAME}" != "index" ];then
                 # split FILE_NAME and get SUB_MENU
                 OIFS=$IFS;IFS=-;FILE_NM_SPLIT=(${FILE_NAME});IFS=$OIFS
                 SUB_MENU=${FILE_NM_SPLIT[0]}
 
-                echo ${FILE_NAME}
-
-                # add nav menu
-                sed -i ".bak" "/_${SUB_MENU}/i\\
-                '${FILE_NAME}'," ${CONFIG_FILE}
+                # find _${SUB_MENU| and insert behind
+                if [[ ${OSTYPE} == "darwin"* ]];then
+                    sed -i ".bak" "/_${SUB_MENU}/a\\
+                    \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ '${FILE_NAME}',
+                    " ${CONFIG_FILE}
+                elif [[ "$OSTYPE" == "linux-gnu" ]];then
+                    sed -i ".bak" "/_${SUB_MENU}/a\\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ '${FILE_NAME}', " ${CONFIG_FILE}
+                fi
             fi
         done
     fi
